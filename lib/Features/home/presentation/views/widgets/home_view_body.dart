fb8b6ebcc32e6d38d2e0095524908f9a94d1e316
mahmoudatta80 <atta89383@gmail.com>
2025-12-21 15:26:58 +0200

    Create All Featured Header ui

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:stylish/Core/widgets/custom_sliver_appbar.dart';
+import 'package:stylish/Features/home/presentation/views/widgets/feature_header.dart';
+import 'package:stylish/Features/home/presentation/views/widgets/search_text_feild.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(slivers: [CustomSliverAppBar()]);
+    return const CustomScrollView(
+      slivers: [
+        CustomSliverAppBar(),
+        SliverToBoxAdapter(child: SearchTextField()),
+        SliverToBoxAdapter(child: FeaturedHeader()),
+      ],
+    );
   }
 }
---
+++ lib/Features/home/presentation/views/widgets/search_text_feild.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stylish/Core/utils/app_text_style.dart';
+
+class SearchTextField extends StatelessWidget {
+  const SearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        borderRadius: BorderRadius.circular(20.r), // Rounded corners
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(
+              context,
+            ).primaryColor.withValues(alpha: .1), // Soft subtle shadow
+            blurRadius: 10.r,
+            spreadRadius: 1,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search any Product..',
+          hintStyle: AppTextStyles.regular14,
+          prefixIcon: Icon(Icons.search, color: Colors.grey, size: 28.r),
+          suffixIcon: Icon(
+            Icons.mic_none_outlined,
+            color: Colors.grey,
+            size: 28.r,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.r),
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.r),
+            borderSide: BorderSide(
+              color: Theme.of(
+                context,
+              ).colorScheme.onSurface.withValues(alpha: 0.3),
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.r),
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.onSurface,
+              width: .5.w,
+            ),
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 15.h),
+        ),
+      ),
+    );
+  }
+}
