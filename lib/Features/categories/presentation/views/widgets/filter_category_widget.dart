b3a6b2807994189957469c5a794a11940399e057
mahmoudatta80 <atta89383@gmail.com>
2026-01-04 14:34:51 +0200

    feat: Dynamically display item count in category filter widget using BlocBuilder and BLoC state.

--- lib/Features/categories/presentation/views/widgets/filter_category_widget.dart
+++ lib/Features/categories/presentation/views/widgets/filter_category_widget.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stylish/Core/utils/app_text_style.dart';
+import 'package:stylish/Features/categories/presentation/manager/category_products_cubit/category_products_cubit.dart';
+import 'package:stylish/Features/categories/presentation/manager/category_products_cubit/category_products_state.dart';
 
 class FilterCategoryWidget extends StatelessWidget {
   const FilterCategoryWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 8.0.h),
-      child: Row(
-        children: [
-          Text('52,456 Items', style: AppTextStyles.bold18),
-          const Spacer(),
-          _buildActionButton(
-            label: 'Sort',
-            icon: Icons.swap_vert,
-            onTap: () {},
-            context: context,
+    return BlocBuilder<CategoryProductsCubit, CategoryProductsState>(
+      builder: (context, state) {
+        int itemsCount = 0;
+        if (state is CategoryProductsSuccess) {
+          itemsCount = state.products.length;
+        } else if (state is CategoryProductsPaginationLoading) {
+          itemsCount = state.products.length;
+        } else if (state is CategoryProductsPaginationFailure) {
+          itemsCount = state.products.length;
+        }
+
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 8.0.h),
+          child: Row(
+            children: [
+              Text(
+                '${itemsCount.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')} Items',
+                style: AppTextStyles.bold18,
+              ),
+              const Spacer(),
+              _buildActionButton(
+                label: 'Sort',
+                icon: Icons.swap_vert,
+                onTap: () {},
+                context: context,
+              ),
+              SizedBox(width: 8.w),
+              _buildActionButton(
+                label: 'Filter',
+                icon: Icons.filter_alt_outlined,
+                onTap: () {},
+                context: context,
+              ),
+            ],
           ),
-          SizedBox(width: 8.w),
-          _buildActionButton(
-            label: 'Filter',
-            icon: Icons.filter_alt_outlined,
-            onTap: () {},
-            context: context,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 
